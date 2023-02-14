826624175cc4edb412fb1c8074b575655817dd84
nhungnq2000 <55863320+alannguyen2705@users.noreply.github.com>
2023-02-14 11:35:42 +0700

    Add files via upload

---
+++ 01a_Program.cs
@@ -0,0 +1,61 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+Card viettel = new();
+viettel.SetProviderName("Viettel");
+viettel.SetPrice(100_000m);
+viettel.SetSerialNumber("10234567895463");
+viettel.SetCode("012345678012304");
+viettel.SetExpirationDate(new(2024, 12, 31));
+viettel.SetDescription($"press toup: *100*code");
+
+Console.WriteLine($"Provider name: {viettel.GetProviderName()}");
+Console.WriteLine($"Price: {viettel.GetPrice()}");
+Console.WriteLine($"Serial Number: {viettel.GetSerialNumber()}");
+Console.WriteLine($"Code Number: {viettel.GetCode()}");
+Console.WriteLine($"Expired Date: {viettel.GetExpirationDate()}");
+Console.WriteLine($"Description: {viettel.GetDescription()}");
+Console.WriteLine($"Is valid: {(viettel.isValid() ? "valid" : "invalid")}");
+
+class Card
+{
+    #region Fields
+    private string providerName;
+    private decimal price;
+    private string serialNumber;
+    private string code;
+    private DateTime expirationDate;
+    private string description;
+    #endregion
+
+    #region Methods
+    public string GetProviderName() => providerName;
+    public void SetProviderName(string providerName) => this.providerName = providerName;
+    public decimal GetPrice() => price;
+    public void SetPrice(decimal price) => this.price = price;
+    public string GetSerialNumber() => serialNumber;
+    public void SetSerialNumber(string serialNumber)
+    {
+        if (serialNumber == null)
+            throw new ArgumentNullException(nameof(serialNumber));
+        if (serialNumber is { Length: not 14 })
+            throw new InvalidDataException("Serial number's must be 14 digits");
+        this.serialNumber = serialNumber;
+    }
+    public string GetCode() => code;
+    public void SetCode(string code)
+    {
+        if (code == null)
+            throw new ArgumentException(nameof(code));
+        if (code is { Length: not 15 })
+            throw new InvalidDataException("Code number's must be 15 digits");
+        this.code = code;
+    }
+    public DateTime GetExpirationDate() => expirationDate;
+    public void SetExpirationDate(DateTime expirationDate) => this.expirationDate = expirationDate;
+    public string GetDescription() => description;
+    public void SetDescription(string description) => this.description = description;
+    public bool isValid() => DateTime.Now <= expirationDate;
+
+    #endregion
+}
---
+++ 01b_Program.cs
@@ -0,0 +1,59 @@
+﻿Card viettel = new();
+viettel.ProviderName = "Viettel";
+viettel.Price = 100_000m;
+viettel.SerialNumber = "10234567853695";
+viettel.Code = "010234567853695";
+viettel.ExpirationDate = new(2022, 12, 3);
+viettel.Description = "Press to upto: *100*code#";
+
+Console.WriteLine($"Provider Name: {viettel.ProviderName}");
+Console.WriteLine($"Price	     : {viettel.Price}");
+Console.WriteLine($"Serial Number: {viettel.SerialNumber}");
+Console.WriteLine($"Code         : {viettel.Code}");
+Console.WriteLine($"Expired Date : {viettel.ExpirationDate}");
+Console.WriteLine($"Description  : {viettel.Description}");
+Console.WriteLine($"Is Valid     : {(viettel.IsValid() ? "valid" : "invalid")}");
+
+class Card
+{
+	#region Fields
+	private string serialNumber;
+	private string code;
+	#endregion
+
+	#region Auto-Implemented Properties
+	public string ProviderName { get; set; }
+	public decimal Price { get; set; }
+	public DateTime ExpirationDate { get; set; }
+	public string Description { get; set; }
+	public string SerialNumber { 
+		get => serialNumber;
+		set
+		{
+			if (value is null)
+				throw new ArgumentNullException("Serial Number's must be required");
+			if (value is { Length: not 14})
+				throw new InvalidDataException("Serial Number's length must be 14 digits");
+
+			serialNumber = value;
+		} 
+	}
+	public string Code { 
+		get => code;
+		set 
+		{
+			if (value is null)
+				throw new ArgumentNullException("Code's must be required");
+			if (value is { Length: not 15 })
+				throw new InvalidDataException("Code's length must be 15 digits");
+			
+			code = value;
+		} 
+	}
+
+	#endregion
+
+	#region Methods
+	public bool IsValid() => DateTime.Now <= ExpirationDate;
+	#endregion
+}
